298f3b65f85ba479547fb4d887cad592c364d870
Ethan Poortenga <eddp@umich.edu>
2022-12-07 18:24:39 -0500

    updating site title and header

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Olakino`,
+    description: `This is Olakino. A virtual character that levels up when you achieve your real-life goals.`,
+    author: `@ethanpoortenga`,
+    siteUrl: `https://olakino.app`,
   },
   plugins: [
     `gatsby-plugin-image`,
