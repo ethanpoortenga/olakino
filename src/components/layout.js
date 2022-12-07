210a94783832ff3cd3c20f95492bc4f28c9c18d0
Ethan Poortenga <eddp@umich.edu>
2022-12-07 17:14:52 -0500

    aligning text to the center

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,15 +24,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
+      <div>s
         <footer
           style={{
             marginTop: `var(--space-5)`,
